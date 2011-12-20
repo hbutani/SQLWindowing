01d3b7b4752b9437f87249ed7679bd718d60a07a
Harish Butani <harish.butani@sap.com>
2011-12-20 15:21:34 -0800

    initial checkin of hiveslave.Windowing Server and Client

---
+++ windowing/src/main/groovy/com/sap/hadoop/windowing/hiveslave/WindowingClient.groovy
@@ -0,0 +1,198 @@
+package com.sap.hadoop.windowing.hiveslave
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.conf.HiveConf;
+
+import com.sap.hadoop.windowing.WindowingException;
+
+class WindowingClient
+{
+	String windowingJar;
+	HiveConf hConf;
+	Process server
+	int serverPort
+	SocketChannel socketChannel;
+	ByteBuffer intb
+	Response resp
+	String serverError
+	
+	WindowingClient(HiveConf hConf, String windowingJar) throws WindowingException
+	{
+		this.hConf = hConf
+		this.windowingJar = windowingJar
+		intb = ByteBuffer.allocate(4)
+		resp = new Response();
+		spawnServer();
+		connect();
+	}
+	
+	void executeQuery(String query) throws WindowingException
+	{
+		validateServer();
+		try
+		{
+			CommUtils.writeString(socketChannel, intb, query);
+			resp.read(socketChannel, intb); 
+		}
+		catch(Exception e)
+		{
+			killServer();
+			throw new WindowingException(e);
+		}
+		
+		switch(resp.type)
+		{
+			case ResponseType.OK: return;
+			case ResponseType.ERROR: throw new WindowingException(resp.errMsg);
+			case ResponseType.QUERY:
+				// todo:
+				// execute hive Query
+				// send Resp.OK
+				// read Resp
+				break;
+			default: throw new WindowingException(sprintf("Unknown ResponseType %s", resp));
+		}
+	}
+	
+	private void spawnServer() throws WindowingException
+	{
+		try
+		{
+			ProcessBuilder pb = new ProcessBuilder(
+				"java", "-cp", windowingJar, 
+				"com.sap.hadoop.windowing.hiveslave.WindowingServer",
+				"-t", "1");
+			Map<String, String> env = pb.environment();
+			ClientUtils.addJobConfToEnvironment(hConf, env);
+			server = pb.start(); 
+		}
+		catch(Exception e)
+		{
+			throw new WindowingException(e);
+		}
+	}
+	
+	private int readPort() throws IOException
+	{
+		DataInputStream scriptIn = new DataInputStream(new BufferedInputStream(
+			server.getInputStream()));
+		return scriptIn.readInt()
+	}
+
+	private void connect() throws WindowingException
+	{
+		try
+		{
+			int port = readPort();
+			InetSocketAddress addr = new InetSocketAddress(port);
+			socketChannel = SocketChannel.open();
+			socketChannel.configureBlocking(true);
+			socketChannel.connect(addr);
+		}
+		catch(IOException ie)
+		{
+			killServer();
+			throw new WindowingException(ie);
+		}
+	}
+	
+	private void startErrorThread()
+	{
+		ServerStreamThread t = new ServerStreamThread(this, server.getErrorStream());
+		t.start();
+	}
+	
+	private void killServer()
+	{
+		if ( server != null )
+		{
+			server.destroy();
+			server = null;
+			socketChannel.close();
+		}
+	}
+	
+	private validateServer() throws WindowingException
+	{
+		if ( serverError != null )
+		{
+			killServer();
+			throw new WindowingException("Server Error:\n" + serverError);
+		}
+	}
+	
+}
+
+class ClientUtils
+{
+	static void addJobConfToEnvironment(Configuration conf, Map<String, String> env) {
+	  Iterator<Map.Entry<String, String>> it = conf.iterator();
+	  while (it.hasNext()) {
+		Map.Entry<String, String> en = it.next();
+		String name = en.getKey();
+		String value = conf.get(name); // does variable expansion
+		name = safeEnvVarName(name);
+		env.put(name, value);
+	  }
+	}
+  
+  /**
+   * addJobConfToEnvironment is shamelessly copied from hadoop streaming.
+   */
+  static String safeEnvVarName(String var) {
+    StringBuilder safe = new StringBuilder();
+    int len = var.length();
+    for (int i = 0; i < len; i++) {
+      char c = var.charAt(i);
+      char s;
+      if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z')
+          || (c >= 'a' && c <= 'z')) {
+        s = c;
+      } else {
+        s = '_';
+      }
+      safe.append(s);
+    }
+    return safe.toString();
+  }
+}
+
+class ServerStreamThread extends Thread
+{
+	WindowingClient client;
+	InputStream inputStream;
+	StringBuilder errorString = new StringBuilder();
+	
+	ServerStreamThread(WindowingClient client, InputStream inputStream)
+	{
+		this.client = client;
+		this.inputStream = inputStream;
+	}
+  
+  
+	public void run()
+	{
+		try
+		{
+			BufferedReader rdr = new BufferedReader(new InputStreamReader(inputStream));
+			String line = null
+			while( (line = rdr.readLine()) != null)
+			{
+				errorString.append(line);
+			}
+			client.serverError = errorString.toString();
+		}
+		catch(IOException ioe)
+		{
+		}
+	}
+}
---
+++ windowing/src/main/groovy/com/sap/hadoop/windowing/hiveslave/WindowingServer.groovy
@@ -0,0 +1,287 @@
+package com.sap.hadoop.windowing.hiveslave
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+import org.apache.hadoop.conf.Configuration;
+
+import com.sap.hadoop.windowing.WindowingException;
+import com.sap.hadoop.windowing.WindowingShell;
+import com.sap.hadoop.windowing.runtime.Mode;
+
+class WindowingServer
+{
+	ServerSocketChannel serverChannel;
+	Selector selector;
+	ExecutorService execSvc;
+	Configuration conf;
+	
+	WindowingServer(int numThreads)
+	{
+		conf = new Configuration();
+		
+		/* setup serverChannel */
+		serverChannel = ServerSocketChannel.open();
+		ServerSocket serverSocket = serverChannel.socket();
+		serverSocket.bind(null);
+		serverChannel.configureBlocking(false);
+		
+		/* setup Selector */
+		selector = Selector.open();
+		serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		
+		/* setup ExecService */
+		execSvc = Executors.newFixedThreadPool(numThreads);
+		
+		// enable invoker to make connection.
+		System.out.println(serverSocket.localPort);
+	}
+	
+	void start()
+	{
+		while(true)
+		{
+			int n = selector.select();
+			
+			if ( n == 0) continue;
+			
+			Iterator it = selector.selectedKeys().iterator();
+			
+			while(it.hasNext())
+			{
+				SelectionKey key = (SelectionKey) it.next();
+				
+				if ( key.isAcceptable() ) handleConnection(key);
+				if ( key.isReadable() ) handleRead(key);
+				
+				// remove key from selected set, so readiness ops recomputed
+				it.remove();
+			}
+		}
+	}
+	
+	void handleConnection(SelectionKey key)
+	{
+		SocketChannel channel = serverChannel.accept();
+		if ( channel != null)
+		{
+			SlaveConnection sConn = new SlaveConnection(conf);
+			channel.configureBlocking(false);
+			channel.register(selector, SelectionKey.OP_READ, sConn)
+		}
+	}
+	
+	void handleRead(final SelectionKey key)
+	{
+		key.interestOps(key.interestOps() & (~SelectionKey.OP_READ))
+		final SlaveConnection conn = (SlaveConnection) key.attachment
+		execSvc.submit({
+			conn.handleRequest(key);
+		})
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		CommandLineParser parser = new PosixParser();
+		Options opts = options();
+		CommandLine cmdLine = parser.parse(opts, args);
+		
+		if ( cmdLine.hasOption('h'))
+		{
+			HelpFormatter hf = new HelpFormatter();
+			hf.printHelp("WindowingServer", opts);
+			System.exit(0);
+		}
+		
+		int numThrds = 1
+		if ( cmdLine.hasOption('t'))
+		{
+			numThrds = Integer.parseInt(cmdLine.getOptionValue('t'));
+		}
+		
+		try
+		{
+			WindowingServer server = new WindowingServer(numThrds);
+			server.start();
+		}
+		catch(Throwable t)
+		{
+			System.err.println(t.getMessage());
+		}
+	}
+
+	
+	static Options options()
+	{
+		Options options = new Options();
+		options.addOption(OptionBuilder.withLongOpt("help").withDescription("help").hasArg(false).create('h'));
+		options.addOption(OptionBuilder.withArgName("numThrds").withLongOpt("threads").withDescription("number threads").hasArg().create('t'));
+		return options;
+	}
+
+}
+
+enum SlaveConnectionState
+{
+	PROCESS_QUERY,
+	PROCESS_RESPONSE;
+}
+
+class SlaveConnection
+{
+	WindowingShell wshell;
+	SlaveConnectionState state;
+	ByteBuffer szb;
+	Response resp;
+	
+	SlaveConnection(Configuration conf)
+	{
+		szb = ByteBuffer.allocate(4)
+		Mode wMode = Mode.MR
+		wshell = new WindowingShell( conf, wMode.getTranslator(), wMode.getExecutor())
+		resp = new Response();
+	}
+	
+	void handleRequest(SelectionKey key)
+	{
+		SocketChannel socketChannel = key.channel();
+		switch(state)
+		{
+			case SlaveConnectionState.PROCESS_QUERY:
+				handleQuery(scoketChannel);
+				break;
+			default:
+				resp.type = ResponseType.ERROR;
+				resp.errMsg = "Internal Error: Connection not in Query State"
+		}
+		resp.write(socketChannel, szb);
+		key.interestOps (key.interestOps() | SelectionKey.OP_READ);
+		key.selector().wakeup();
+	}
+	
+	void handleQuery(SocketChannel socketChannel)
+	{
+		String qry = CommUtils.readString(socketChannel, szb)
+		try
+		{
+			wshell.execute(qry); // todo: pass an Object to wshell that can be used to execute Hive Queries
+			resp.type = ResponseType.OK;
+		}
+		catch(WindowingException e)
+		{
+			resp.type = ResponseType.ERROR;
+			resp.errMsg = e.getMessage();
+		}
+		resp.write(socketChannel, szb);
+	}
+	
+	
+	
+	
+}
+
+enum ResponseType
+{
+	OK,
+	QUERY,
+	ERROR;
+	
+	static ResponseType fromInt(int i)
+	{
+		switch(i)
+		{
+			case 0: return OK;
+			case 1: return QUERY;
+			case 2: return ERROR;
+			default: throw new RuntimeException(sprintf("Unknown ResponseType %d", i));
+		}
+	}
+	
+	static int toInt(ResponseType r)
+	{
+		switch(r)
+		{
+			case OK: return 0;
+			case QUERY: return 1;
+			case ERROR: return 2;
+			default: throw new RuntimeException(sprintf("Unknown ResponseType %s", r));
+		}
+	}
+}
+
+class Response
+{
+	ResponseType type;
+	String query;
+	String errMsg;
+	
+	void read(SocketChannel socketChannel, ByteBuffer intb)
+	{
+		type = ResponseType.fromInt(CommUtils.readInt(socketChannel, intb));
+		switch(type)
+		{
+			case ResponseType.OK: break;
+			case ResponseType.QUERY: query = CommUtils.readString(socketChannel, intb);
+			case ResponseType.ERROR: errMsg = CommUtils.readString(socketChannel, intb);
+			default: throw new RuntimeException(sprintf("Unknown ResponseType %s", this));
+		}
+	}
+	
+	void write(SocketChannel socketChannel, ByteBuffer intb)
+	{
+		CommUtils.writeInt(socketChannel, intb, ResponseType.toInt(type));
+		switch(type)
+		{
+			case ResponseType.OK: break;
+			case ResponseType.QUERY: CommUtils.writeString(socketChannel, intb, query);
+			case ResponseType.ERROR: CommUtils.writeString(socketChannel, intb, errMsg);
+			default: throw new RuntimeException(sprintf("Unknown ResponseType %s", this));
+		}
+	}
+}
+
+class CommUtils
+{
+	static int readInt(SocketChannel socketChannel, ByteBuffer intb)
+	{
+		intb.reset();
+		while (intb.hasRemaining() ) socketChannel.read(intb);
+		intb.flip();
+		return intb.getInt();
+	}
+	
+	static String readString(SocketChannel socketChannel, ByteBuffer intb)
+	{
+		int sz = readInt(socketChannel, intb);
+		ByteBuffer strb = ByteBuffer.allocate(sz);
+		while(strb.hasRemaining()) socketChannel.read(strb);
+		return new String(strb.array())
+	}
+	
+	static void writeInt(SocketChannel socketChannel, ByteBuffer intb, int i)
+	{
+		intb.reset();
+		intb.putInt(i);
+		intb.flip();
+		while (intb.hasRemaining() ) socketChannel.write(intb);
+	}
+	
+	static void writeString(SocketChannel socketChannel, ByteBuffer intb, String s)
+	{
+		byte[] b = s.getBytes();
+		writeInt(socketChannel, intb, b.length)
+		ByteBuffer strb = ByteBuffer.wrap(b)
+		while (strb.hasRemaining() ) socketChannel.write(strb);
+	}
+}

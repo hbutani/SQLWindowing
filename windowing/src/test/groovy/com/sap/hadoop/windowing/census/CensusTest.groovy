6cd3b33610761274798451e4839ec3a370d28fa3
Harish Butani <rhbutani@gmail.com>
2012-01-04 10:21:06 -0800

    Complete - task : Setup census schema and load data

--- windowing/src/test/groovy/com/sap/hadoop/windowing/census/CensusTest.groovy
+++ windowing/src/test/groovy/com/sap/hadoop/windowing/census/CensusTest.groovy
@@ -12,7 +12,7 @@ class CensusTest extends MRBaseTest
 	 * @CTAS create table census_q1 as select county, tract, arealand from geo_header_sf1 where sumlev = 140 ;
 	 */
 	@Test
-	void testRC()
+	void testQ1()
 	{
 		wshell.execute("""
 		from census_q1 

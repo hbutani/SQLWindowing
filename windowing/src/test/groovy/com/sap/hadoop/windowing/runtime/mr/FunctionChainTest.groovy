4d1d9b6150a38e0a2aebf8886c995d327f1270ff
Harish Butani <rhbutani@gmail.com>
2012-03-28 10:06:26 -0700

    function chain testing

---
+++ windowing/src/test/groovy/com/sap/hadoop/windowing/runtime/mr/FunctionChainTest.groovy
@@ -0,0 +1,32 @@
+package com.sap.hadoop.windowing.runtime.mr;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sap.hadoop.windowing.MRBaseTest;
+import com.sap.hadoop.windowing.query.Query;
+
+class FunctionChainTest extends MRBaseTest
+{	
+	@Test
+	void test1()
+	{
+		int jobId = System.currentTimeMillis()
+		String jobWorkingDir = JobBase.getJobWorkingDir(wshell.cfg, jobId)
+		
+		wshell.execute("""
+		from noop(
+               noop(
+                 noop(part_rc partition by p_mfgr order by p_mfgr, p_name)
+			   ) partition by p_mfgr order by p_mfgr, p_name
+             ) partition by p_mfgr order by p_mfgr, p_name
+with
+  rank() as r
+select p_mfgr,p_name, p_size, r
+		into path='/tmp/wout'
+		serde 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+		format 'org.apache.hadoop.mapred.TextOutputFormat'""")
+	}
+}

3bd28cf50e01a7c1491b5e8bf670e341b01eaadd
Harish Butani <rhbutani@gmail.com>
2012-05-26 18:03:40 -0700

    more Dynamic Item Counting testing

--- windowing/src/test/groovy/com/sap/hadoop/windowing/functions/marketbasket/MarketBasketAnalysisLocalTest.groovy
+++ windowing/src/test/groovy/com/sap/hadoop/windowing/functions/marketbasket/MarketBasketAnalysisLocalTest.groovy
@@ -21,7 +21,30 @@ class MarketBasketAnalysisLocalTest  extends BaseTest {
 				 'columns.types' = 'string,string',
 				 'field.delim'=','
 					) partition by itemset order by itemset,
-			  'basketName', 'itemName', <0.05>)
+			  'basketName', 'itemName', <0.15>)
+		select itemset
+		""")
+
+		String r = outStream.toString()
+		r = r.replace("\r\n", "\n")
+		println r
+	}
+	
+	@Test
+	void testDb10000() {
+		wshell.execute("""
+	from candidateFrequentItemSets(tableinput(
+				 recordreaderclass='com.sap.hadoop.windowing.io.TableWindowingInput',
+				 keyClass='org.apache.hadoop.io.Text',
+				 valueClass='org.apache.hadoop.io.Text',
+				 inputPath='$basedir/data/marketbasket/db10000',
+				 inputformatClass='org.apache.hadoop.mapred.TextInputFormat',
+				 serdeClass='org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe',
+				 columns = 'basketName,itemName',
+				 'columns.types' = 'string,string',
+				 'field.delim'=','
+					) partition by itemset order by itemset,
+			  'basketName', 'itemName', <0.20>)
 		select itemset
 		""")
 
--- windowing/src/test/groovy/com/sap/hadoop/windowing/functions/marketbasket/MarketBasketAnalysisTest.groovy
+++ windowing/src/test/groovy/com/sap/hadoop/windowing/functions/marketbasket/MarketBasketAnalysisTest.groovy
@@ -18,13 +18,51 @@ class MarketBasketAnalysisTest extends MRBaseTest
 	
 	@Test
 	void test1() {
-		wshell.execute("""
-			from candidateFrequentItemSets(basketdata partition by itemset order by itemset,
-			  'basketName', 'itemName', <0.05>)
-			select itemset
-		into path='/tmp/wout2'
-		serde 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
-		with serdeproperties('field.delim'=',')
-		format 'org.apache.hadoop.mapred.TextOutputFormat'""")
+		String reds = wshell.cfg.get(" mapred.reduce.tasks");
+		try
+		{			
+			wshell.cfg.set(" mapred.reduce.tasks", "1");
+			wshell.execute("""
+				from candidateFrequentItemSets(
+				   <select * from basketdata 
+					distribute by basketName
+					sort by basketName, itemName>
+				   partition by itemset order by itemset,
+				  'basketName', 'itemName', <0.15>)
+				select itemset
+			into path='/tmp/wout2'
+			serde 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+			with serdeproperties('field.delim'=',')
+			format 'org.apache.hadoop.mapred.TextOutputFormat'""")
+		}
+		finally
+		{
+			wshell.cfg.set(" mapred.reduce.tasks", reds);
+		}
+	}
+	
+	@Test
+	void testDb1000() {
+		String reds = wshell.cfg.get(" mapred.reduce.tasks");
+		try
+		{
+			wshell.cfg.set(" mapred.reduce.tasks", "1");
+			wshell.execute("""
+				from candidateFrequentItemSets(
+				   <select * from basketdata_db10000
+					distribute by basketName
+					sort by basketName, itemName>
+				   partition by itemset order by itemset,
+				  'basketName', 'itemName', <0.2>)
+				select itemset
+			into path='/tmp/wout2'
+			serde 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+			with serdeproperties('field.delim'=',')
+			format 'org.apache.hadoop.mapred.TextOutputFormat'""")
+		}
+		finally
+		{
+			wshell.cfg.set(" mapred.reduce.tasks", reds);
+		}
 	}
 }

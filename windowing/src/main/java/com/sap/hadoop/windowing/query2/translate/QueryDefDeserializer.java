65b8e760ccbd07d90af5465842b737609d33167f
Harish Butani <rhbutani@gmail.com>
2012-10-03 11:27:20 -0700

    introduce QueryDefWalker and QueryDefVisitor skeleton for QueryDeSerializer

---
+++ windowing/src/main/java/com/sap/hadoop/windowing/query2/translate/QueryDefDeserializer.java
@@ -0,0 +1,60 @@
+package com.sap.hadoop.windowing.query2.translate;
+
+
+
+/*
+ * Deserialization Steps:
+ * 0. Assume HiveConf is passed to this Object on construction.
+ * On initialize:
+ * - create TranslationInfo and set on QDef
+ * - setup HiveCfg and Hive on TranslationInfo
+ * On HiveTableDef:
+ * - set OI and SerDe using:
+  		serDe = (SerDe) SerDeUtils.lookupDeserializer(serDeClassName);
+		serDe.initialize(conf, tbl);
+	// may require adding columns and columnTypes to Properties. See OutputTranslation:setupOutputSerDe line 243
+ * - add the inputDef to the TranslationInfo, so it add it to the InfoMap.
+ * On HiveQueryDef:
+ * - same as HiveTableDef
+ * 
+ * On TableFuncDef previsit:
+ * - have a instance variable that holds onto the InputInfo for this TableFunc
+ * - have another instance variable that holds onto the map-side InputInfo for this TableFunc(if TFunc has Map Side processing)
+ *   if TblFunc has no map-side set this to the InputInfo from above.
+ * - invoke setupMapOI on the TableFunctionEvaluator; enable TFunc to establish MapOI
+ * - if TblFunc has a Map Phase:
+ *   - invoke tInfo.getMapInputInfo(tFnDef) // this triggers creating and storing the Map-side InputInfo in TranslationInfo
+ *   - set this InputInfo as the current map InputInfo.
+ * on TblFn Args:
+ *   - recreate the ExprEvaluator, OI using the current mapInputInfo
+ * on TblFn Window:
+ *   - for Partition:
+ *     - recreate the ExprEvaluator, OI using the current mapInputInfo
+ *   - for Order:
+ *     - recreate the ExprEvaluator, OI using the current mapInputInfo
+ *   - for ValueBoundary:
+ *     - recreate the ExprEvaluator, OI using the current mapInputInfo
+ * for WdwTablFunc:
+ *   - for WndFnDef
+ *     - for Args:
+ *       - recreate the ExprEvaluator, OI using the current InputInfo
+ *     - process WdwDef just like TblFunc's Wdw but use the current InputInfo.
+ *     - invoke init on the GenericUDAFEvaluator and set the OI.
+ * On TableFuncDef visit:
+ * - invoke setupOI on the TableFunctionEvaluator
+ * - set Serde, OI, and MapOI on tDef: see end of InputTranslation:translate(TblFuncDef) method, from line 221
+ * 
+ * On WhereDef:
+ * - recreate ExprNodeEvaluator, OI using InputInfo of first InputDef in chain.
+ * On SelectDef:
+ * - On ColumnDef:
+ *   - recreate ExprNodeEvaluator, OI using InputInfo of first InputDef in chain.
+ * - On QueryOutputDef:
+ *   - set SerDe using:
+  		serDe = (SerDe) SerDeUtils.lookupDeserializer(serDeClassName);
+		serDe.initialize(conf, tbl);
+ */
+public class QueryDefDeserializer extends QueryDefVisitor
+{
+
+}
---
+++ windowing/src/main/java/com/sap/hadoop/windowing/query2/translate/QueryDefWalker.java
@@ -0,0 +1,193 @@
+package com.sap.hadoop.windowing.query2.translate;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.sap.hadoop.windowing.WindowingException;
+import com.sap.hadoop.windowing.functions2.FunctionRegistry;
+import com.sap.hadoop.windowing.query2.definition.ArgDef;
+import com.sap.hadoop.windowing.query2.definition.ColumnDef;
+import com.sap.hadoop.windowing.query2.definition.HiveQueryDef;
+import com.sap.hadoop.windowing.query2.definition.HiveTableDef;
+import com.sap.hadoop.windowing.query2.definition.OrderColumnDef;
+import com.sap.hadoop.windowing.query2.definition.OrderDef;
+import com.sap.hadoop.windowing.query2.definition.PartitionDef;
+import com.sap.hadoop.windowing.query2.definition.QueryDef;
+import com.sap.hadoop.windowing.query2.definition.QueryInputDef;
+import com.sap.hadoop.windowing.query2.definition.SelectDef;
+import com.sap.hadoop.windowing.query2.definition.TableFuncDef;
+import com.sap.hadoop.windowing.query2.definition.WindowDef;
+import com.sap.hadoop.windowing.query2.definition.WindowFrameDef;
+import com.sap.hadoop.windowing.query2.definition.WindowFrameDef.BoundaryDef;
+import com.sap.hadoop.windowing.query2.definition.WindowFrameDef.CurrentRowDef;
+import com.sap.hadoop.windowing.query2.definition.WindowFrameDef.RangeBoundaryDef;
+import com.sap.hadoop.windowing.query2.definition.WindowFrameDef.ValueBoundaryDef;
+import com.sap.hadoop.windowing.query2.definition.WindowFunctionDef;
+
+/**
+ * encapsulate the Order in which a QueryDef should be processed.
+ *
+ */
+public class QueryDefWalker
+{
+	QueryDefVisitor visitor;
+	
+	public QueryDefWalker()
+	{
+		super();
+	}
+
+	public QueryDefWalker(QueryDefVisitor visitor)
+	{
+		super();
+		this.visitor = visitor;
+	}
+	
+	public void walk(QueryDef qDef) throws WindowingException
+	{
+		visitor.initialize(qDef);
+		walkInputChain(qDef);
+		
+		if ( qDef.getWhere() != null )
+		{
+			visitor.visit(qDef.getWhere());
+		}
+		
+		walk(qDef.getSelectList());
+		visitor.visit(qDef.getOutput());
+		
+		visitor.finish();
+	}
+	
+	protected void walkInputChain(QueryDef qDef) throws WindowingException
+	{
+		Iterator<QueryInputDef> it = TranslateUtils.iterateInputDefs(qDef, true);
+		while(it.hasNext())
+		{
+			QueryInputDef nextDef = it.next();
+			if (nextDef instanceof HiveTableDef)
+			{
+				visitor.visit((HiveTableDef) nextDef);
+			}
+			else if (nextDef instanceof HiveQueryDef)
+			{
+				visitor.visit((HiveQueryDef) nextDef);
+			}
+			else
+			{
+				walk(qDef, (TableFuncDef) nextDef);
+			}
+		}
+	}
+	
+	protected void walk(QueryDef qDef, TableFuncDef tblFunc) throws WindowingException
+	{
+		// 1. allow visitor to establish input
+		visitor.preVisit(tblFunc);
+		
+		// 2. visit the Args.
+		walk(tblFunc.getArgs());
+		
+		//3. walk the window objects
+		walk(tblFunc.getWindow());
+		
+		//4. for WindowTable Func walk the Window Functions on the Select List
+		String fName = tblFunc.getName();
+		if ( fName.equals(FunctionRegistry.WINDOWING_TABLE_FUNCTION))
+		{
+			SelectDef select = qDef.getSelectList();
+			ArrayList<WindowFunctionDef> wFns = select.getWindowFuncs();
+			for(WindowFunctionDef wFn : wFns)
+			{
+				walk(wFn.getWindow());
+				walk(wFn.getArgs());
+				visitor.visit(wFn);
+			}
+		}
+		
+		//5. revisit tblFunc
+		visitor.visit(tblFunc);
+	}
+	
+	protected void walk(ArrayList<ArgDef> args) throws WindowingException
+	{
+		if ( args != null )
+		{
+			for(ArgDef arg :args)
+			{
+				visitor.visit(arg);
+			}
+		}
+	}
+	
+	protected void walk(WindowDef window) throws WindowingException
+	{
+		if ( window == null ) return;
+		
+		PartitionDef pDef = window.getPartDef();
+		ArrayList<ColumnDef> cols = pDef.getColumns();
+		for(ColumnDef col : cols)
+		{
+			visitor.visit(col);
+		}
+		visitor.visit(pDef);
+		
+		OrderDef oDef = window.getOrderDef();
+		ArrayList<OrderColumnDef> ocols = oDef.getColumns();
+		for(OrderColumnDef ocol : ocols)
+		{
+			visitor.visit(ocol);
+		}
+		visitor.visit(oDef);
+		
+		WindowFrameDef wFrmDef = window.getWindow();
+		if ( wFrmDef != null)
+		{
+			walk(wFrmDef.getStart());
+			walk(wFrmDef.getEnd());
+			visitor.visit(wFrmDef);
+		}
+		visitor.visit(window);
+	}
+	
+	protected void walk(BoundaryDef boundary) throws WindowingException
+	{
+		if ( boundary instanceof ValueBoundaryDef )
+		{
+			visitor.visit((ValueBoundaryDef)boundary);
+		}
+		else if ( boundary instanceof RangeBoundaryDef)
+		{
+			visitor.visit((RangeBoundaryDef)boundary);
+		}
+		else if ( boundary instanceof CurrentRowDef)
+		{
+			visitor.visit((CurrentRowDef)boundary);
+		}
+	}
+	
+	protected void walk(SelectDef select) throws WindowingException
+	{
+		ArrayList<ColumnDef> cols = select.getColumns();
+		if ( cols != null )
+		{
+			for(ColumnDef col : cols)
+			{
+				visitor.visit(col);
+			}
+		}
+		visitor.visit(select);
+	}
+	
+	
+	public QueryDefVisitor getVisitor()
+	{
+		return visitor;
+	}
+	
+	public void setVisitor(QueryDefVisitor visitor)
+	{
+		this.visitor = visitor;
+	}
+	
+}
